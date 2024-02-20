3d855640f3302f63e599b3b999a499fbab500eac
Cromizone <cromizonebolt@gmail.com>
2024-02-20 17:02:41 +0530

    ⬆️  improved code quality of the code base, removed username field from pydantic models

--- crypt/interface.py
+++ crypt/interface.py
@@ -1,10 +1,13 @@
 from hashlib import md5
 from pathlib import Path
 
-import typer
-from rich import print, prompt, table
+from rich import print
+from rich.prompt import Prompt, Confirm
 
-from models import Store, Account, InitialVectors
+import typer
+
+import models
+
 
 storefile = Path(__file__).parent.joinpath("store.json").resolve()
 app = typer.Typer(
@@ -15,30 +18,32 @@ app = typer.Typer(
 )
 
 
-@app.command(
-    "init", short_help="create store", help="intionalizes a store with master password hash"
-)
+def error(message: str, code: int = 1) -> None:
+    print(f"[red][b]error:[reset] {message}")
+    raise typer.Exit(code=code)
+
+
+@app.command("init", short_help="create store", help="intionalizes a store with master password hash")
 def register() -> None:
     if not storefile.exists():
-        print(f"[green]creating store file at [magenta][i]'{storefile}'")
+        print(f"creating store file at [magenta][i]'{storefile}'")
         storefile.touch()
 
-    while True:
-        master_passphrase = prompt.Prompt.ask(
-            ":locked_with_key: please enter a password [magenta](must be at least 5 characters)",
-            password=True
-        )
+    elif not storefile.read_text() == "" and not Confirm.ask("[yellow]overwrite existing store"):
+        error("overwrite permission denied", code=2)
 
+    while True:
+        master_passphrase = Prompt.ask(":locked_with_key: enter new master passphrase [magenta](must be at least 5 characters)", password=True)
         if len(master_passphrase) >= 5:
             break
-        print("[prompt.invalid]password too short")
+
+        print("[prompt.invalid]passphrase too short")
 
     keyhash = md5(master_passphrase.encode()).hexdigest()
-    print(f"password signature: [b]{keyhash}")
+    print(f"passphrase signature: [b]{keyhash}")
 
-    store = Store(keyhash=keyhash)
+    store = models.Store(keyhash=keyhash)
+    store_json = store.model_dump_json(indent=2)
 
-    print("[yellow]updating store file")
-    storefile.write_text(store.model_dump_json(indent=2))
-
-    print(f"[green][b]Successfuly intionalized store")
+    storefile.write_text(store_json)
+    print("[green][b]Successfuly intionalized store")
--- crypt/models.py
+++ crypt/models.py
@@ -1,18 +1,10 @@
 from pydantic import BaseModel, Field
 
-
-class InitialVectors(BaseModel):
-    username: str
-    passphrase: str
-
-
 class Account(BaseModel):
-    salt: str
-    initial_vectors: InitialVectors
-
-    username: str
     passphrase: str
 
+    salt: str
+    initial_vectors: str
 
 class Store(BaseModel):
     keyhash: str
