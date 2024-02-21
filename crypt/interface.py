1ef85a99def1c9103f998cbc78697988402b2762
Cromizone <cromizonebolt@gmail.com>
2024-02-21 12:36:40 +0530

    ⬆️  improved code quality of the code base and improved error messages

--- crypt/interface.py
+++ crypt/interface.py
@@ -29,14 +29,14 @@ def error(message: str, code: int = 1) -> None:
     raise typer.Exit(code=code)
 
 
-@app.command("init", short_help="create store", help="intionalizes a store with master password hash")
+@app.command("init", short_help="Initiate store.", help="Initiate a store in the application directory with the hashed form of the master passphrase.")
 def register() -> None:
     if not storefile.exists():
         print(f"creating store file at [magenta][i]'{storefile}'")
         storefile.touch()
 
-    elif not storefile.read_text() == "" and not Confirm.ask("[yellow]overwrite existing store"):
-        error("overwrite permission denied", code=2)
+    if not storefile.read_text() == "" and not Confirm.ask("[yellow]overwrite existing store"):
+        error("overwrite permission denied", code=3)
 
     while True:
         master_passphrase = Prompt.ask(":locked_with_key: enter new master passphrase [magenta](must be at least 5 characters)", password=True)
@@ -49,35 +49,35 @@ def register() -> None:
     print(f"passphrase signature: [b]{keyhash}")
 
     store = models.Store(keyhash=keyhash)
-    store_json = store.model_dump_json(indent=2)
+    store_json = store.model_dump_json(indent=3)
 
     storefile.write_text(store_json)
     print("[green][b]successfully initialized store")
 
 
-@app.command("add", short_help="insert account", help="insert account with passphrase into the store")
+@app.command("add", short_help="Add an account.", help="Add an account to the store encrypted using the AES encryption method.")
 def add(
-    target: Annotated[str, typer.Argument(show_default=False, dir_okay=False, file_okay=False)],
+    site: Annotated[str, typer.Argument(show_default=False, dir_okay=False, file_okay=False)],
     overwritable: Annotated[bool, typer.Option("--overwrite", "-o", show_default=False, is_flag=True)] = False,
 ) -> None:
     if not storefile.exists():
-        error("store file not initialized", code=3)
+        error("the store file has not been initialized, use the [i]'init'[/] command to initialize the store", code=4)
 
     store = models.Store.model_validate_json(storefile.read_text())
 
-    if store.accounts.__contains__(target) and not overwritable:
-        error("account already exists", code=1)
+    if store.accounts.__contains__(site) and not overwritable:
+        error("the account already exists within the store, use the [i]'--overwrite'[/] flag to overwrite the existing account", code=2)
 
-    if overwritable and store.accounts.__contains__(target) and not Confirm.ask("[yellow]overwrite existing account"):
-        error("overwrite permission denied", code=2)
+    if overwritable and store.accounts.__contains__(site) and not Confirm.ask("[yellow]overwrite existing account"):
+        error("overwrite permission denied", code=3)
 
     master_passphrase = Prompt.ask(":locked_with_key: enter master passphrase", password=True)
     if not md5(master_passphrase.encode()).hexdigest() == store.keyhash:
-        error("incorrect master passphrase", code=4)
+        error("incorrect master passphrase", code=5)
 
     passphrase = Prompt.ask(":pencil: enter account passphrase [magenta](should be at least 5 characters)", password=True)
 
-    print(f"account passphrase signature: [b]{md5(passphrase.encode()).hexdigest()}")
+    print(f"passphrase signature: [b]{md5(passphrase.encode()).hexdigest()}")
     
     salt = cryptography.generate_salt()
     passphrase_iv = cryptography.generate_iv()
@@ -85,70 +85,65 @@ def add(
 
     account = models.Account(passphrase=passphrase, salt=salt, initial_vector=passphrase_iv)
 
-    store.accounts[target] = account
+    store.accounts[site] = account
 
-    store_json = store.model_dump_json(indent=2)
+    store_json = store.model_dump_json(indent=3)
 
     storefile.write_text(store_json)
-    print(f"[green][b]successfully registered an account for [i]{target}")
+    print(f"[green][b]registered [i]{site}[/] account")
 
-@app.command("get", short_help="get account passphrase", help="get/print account passphrase from store")
+@app.command("get", short_help="Get passphrase.", help="Display or copy the passphrase of any account in the store.")
 def get(
-    target: Annotated[str, typer.Argument(show_default=False, dir_okay=False, file_okay=False)],
-    clipboard: Annotated[bool, typer.Option("--clipboard", "-c", show_default=False, is_flag=True)] = False,
+    site: Annotated[str, typer.Argument(show_default=False, dir_okay=False, file_okay=False)],
+    copy: Annotated[bool, typer.Option("--copy", "-c", show_default=False, is_flag=True)] = False,
 ) -> None:
     if not storefile.exists():
-        error("store file not initialized", code=3)
+        error("the store file has not been initialized, use the [i]'init'[/] command to initialize the store", code=4)
 
     store = models.Store.model_validate_json(storefile.read_text())
 
-    if not store.accounts.__contains__(target):
-        error("account doesn't exists", code=1)
+    if not store.accounts.__contains__(site):
+        error("The account does not exist. To add a new account, use the [i]'add'[/] command.", code=2)
 
     master_passphrase = Prompt.ask(":locked_with_key: enter master passphrase", password=True)
     if not md5(master_passphrase.encode()).hexdigest() == store.keyhash:
-        error("incorrect master passphrase", code=4)
+        error("incorrect master passphrase", code=5)
 
-    account = store.accounts[target]
+    account = store.accounts[site]
+    passphrase = cryptography.decrypt(account.passphrase, master_passphrase, account.salt, account.initial_vector)
 
-    try:
-        passphrase = cryptography.decrypt(account.passphrase, master_passphrase, account.salt, account.initial_vector)
-
-    except BaseException:
-        error("decryption failed, possibly because given master passphrase was not used at encryption", code=5)
-
-    if not clipboard:
+    if not copy:
         print(f"[black]passphrase: [yellow][b]{passphrase}")
     else:
         pyperclip.copy(passphrase)
         print("[green][b]passphrase copied to clipboard")
 
-@app.command("remove", short_help="remove account", help="remove an account from store")
+@app.command("remove", short_help="Remove an account.", help="Remove an account from store.")
 def remove(
-    target: Annotated[str, typer.Argument(show_default=False, dir_okay=False, file_okay=False)],
-    ask: Annotated[bool, typer.Option("--no-input", "-ni", show_default=False, is_flag=True)] = True,
+    site: Annotated[str, typer.Argument(show_default=False, dir_okay=False, file_okay=False)],
+    confirmation: Annotated[bool, typer.Option("--no-input", "-ni", show_default=False, is_flag=True)] = True,
 ) -> None:
     if not storefile.exists():
-        error("store file not initialized", code=3)
+        error("the store file has not been initialized, use the [i]'init'[/] command to initialize the store", code=4)
 
     store = models.Store.model_validate_json(storefile.read_text())
 
-    if not store.accounts.__contains__(target):
-        error("account doesn't exists", code=1)
+    if not store.accounts.__contains__(site):
+        error("The account does not exist. To add a new account, use the [i]'add'[/] command.", code=2)
 
-    if ask and not Confirm.ask("[yellow]remove account from store"):
-        error("delete permission not granted by user", code=2)
+    if confirmation and not Confirm.ask("[yellow]remove account from store"):
+        error("permission denied", code=3)
 
-    store.accounts.pop(target)
-    store_json = store.model_dump_json(indent=2)
+    store.accounts.pop(site)
+    store_json = store.model_dump_json(indent=3)
 
     storefile.write_text(store_json)
-    print(f"[green][b]account for {target} was removed from store")
+    print(f"[green][b]account [i]{site}[/] removed")
 
-@app.command("list", short_help="list accounts", help="list every accounts from store")
-def ls():
+@app.command("list", short_help="List accounts.", help="list every accounts from store")
+def show():
     if not storefile.exists():
-        error("store file not initialized", code=3)
+        error("the store file has not been initialized, use the [i]'init'[/] command to initialize the store", code=4)
 
     store = models.Store.model_validate_json(storefile.read_text())
 
